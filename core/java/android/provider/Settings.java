dba9e7a3b2536e2d4869bd8af75b55b04cbd9d58
Moludos <moledo.alex@gmail.com>
2015-12-15 03:06:34 +0100

    Add three-fingers-swipe to screenshot [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3758,6 +3758,14 @@ public final class Settings {
          */
         public static final String HEADS_UP_FORCE_ALL = "heads_up_force_all";
 
+ 	/**
+         * Three Finger Gesture from Oppo
+         *
+         * @hide
+         */
+        public static final String THREE_FINGER_GESTURE = "three_finger_gesture";
+
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
