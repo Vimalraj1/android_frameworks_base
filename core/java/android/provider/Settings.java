48a56f573c8f20e19f9ab8d0aa3b430058042f49
David <davidlglozo@gmail.com>
2016-05-17 22:32:37 +0200

    Hide supersu icon statusbar[1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3677,6 +3677,11 @@ public final class Settings {
         public static final String STATUSBAR_CLOCK_COLOR = "statusbar_clock_color";
 
         /**
+         * @hide
+         */
+        public static final String SHOW_SU_INDICATOR = "show_su_indicator";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
